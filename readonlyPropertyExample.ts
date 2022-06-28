9515cad55f5b3b4ab0078c44f90b3b2a308c2c17
developersung13 <developersung13@gmail.com>
2022-06-28 20:24:35 +0900

    Upload (2022/06/28)

--- readonlyPropertyExample.ts
+++ readonlyPropertyExample.ts
@@ -2,4 +2,4 @@ function addTwoNumbers(config: { readonly a: number; readonly b: number }) {
   return config.a + config.b;
 }
 
-console.log(addTwoNumbers({ a: 100, b: 200 }));
+console.log(addTwoNumbers({ a: 100, b: 200 })); // 300
